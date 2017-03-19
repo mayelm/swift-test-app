959a1ce4e383ee9a1f99e670f4dce4a816887f46
Cintia <mayelm@yahoo.com>
2017-03-19 15:15:34 +1100

    First test commiting

--- pininos dos/ViewController.swift
+++ pininos dos/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var labelhello: UILabel!
 
+    var tapCount = 0
+    
+    
+    @IBAction func buttontapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            labelhello.text = "You pressed the button 10 times!"
+        }
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
